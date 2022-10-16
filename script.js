3828785bf127f67ce6ef7887758eadf958933e5c
Frans-Budi <fransbudikashira@gmail.com>
2022-10-16 21:16:11 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const menu = document.querySelector('.hambuger-bar input');
+const nav1 = document.querySelector('header .download');
+const nav2 = document.querySelector('header .media-sosial');
+const nav3 = document.querySelector('header .navigator');
+
+menu.addEventListener('click', function () {
+    nav1.classList.toggle('slide');
+    nav2.classList.toggle('slide');
+    nav3.classList.toggle('slide');
+});
+
+document.querySelectorAll('header .hambuger-bar a').forEach(n => n.
+    addEventListener('click', () => {
+        nav1.classList.remove('slide');
+        nav2.classList.remove('slide');
+        nav3.classList.remove('slide');
+    }))
+
+
+const header = document.querySelector('header');
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+    if (lastScrollY < window.scrollY) {
+        console.log("We are going Down");
+    } else {
+        console.log("We are going Up");
+    }
+}
+
+);
